ea01150922fcc0b314378b7e89b48e78471791a0
Shawn M Moore <sartak@gmail.com>
2009-05-17 03:59:26 -0400

    new_from_line

--- lib/NetHack/Logfile/Entry.pm
+++ lib/NetHack/Logfile/Entry.pm
@@ -7,6 +7,30 @@ field version => (
     isa => 'Str',
 );
 
+sub new_from_line {
+    my $self = shift;
+    my $line = shift;
+
+    my ($version) = $line =~ /^([\d\.]+) /;
+    confess "Unable to determine NetHack version from the log line '$version'"
+        if !$version;
+
+    my $original_version = $version;
+
+    $version =~ tr/.//;
+    while ($version) {
+        my $package = "NetHack::Logfile::Entry::$version";
+        if (eval { Class::MOP::load_class($package); 1 }) {
+            return $package->_parse_and_construct($line);
+        }
+        warn $@ if $@ !~ /^Can't locate NetHack/;
+
+        chop $version;
+    }
+
+    confess "This version of NetHack::Logfile cannot handle NetHack version $original_version log entries.";
+}
+
 __PACKAGE__->meta->make_immutable;
 
 1;
